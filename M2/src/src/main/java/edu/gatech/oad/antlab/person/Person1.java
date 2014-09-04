e2980bbd4fac12f505dd0de69fdf37fa428e0e79
tsolanki95 <tsolanki95@yahoo.in>
2014-09-04 16:15:30 -0400

    Tanmay Solanki- completed the calc() method in the person1.java class according to the javadoc

--- M2/src/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -9,41 +9,47 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
+    /** Holds the persons real name */
+    private String name;
+    
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+    public Person1(String pname) {
+        name = pname;
+    }
+    /**
+     * This method should take the string
+     * input and return its characters rotated
+     * 2 positions.
+     * given "gtg123b" it should return
+     * "g123bgt".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        String output = "";
+        for (int i = 2; i < input.length(); i++) {
+            output = output + input.charAt(i);
+        }
+        output = output + input.charAt(0);
+        output = output + input.charAt(1);
+        return output;
+    }
 	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the 
+     *         object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 
 }
